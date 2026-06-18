fbc8a97f6e7897e92e788d19864156e8fea70df9
Franco Genoff <francogenoff@gmail.com>
2026-06-18 00:49:49 +0000

    Aplicando solamente estilos a la calculadora con CSS

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const valor1 = "";
+const valor2 = "";
+const operador = "";
+
+function suma(valor1, valor2) {
+    return valor1 + valor2;
+}
+
+function resta(valor1, valor2) {
+    return valor1 - valor2;
+}
+
+function division(valor1, valor2) {
+    return valor1 / valor2;
+}
+
+function multiplicacion(valor1, valor2) {
+    return valor1 * valor2;
+}
+
+
+function operacion(valor1, operador, valor2) {
+
+}
